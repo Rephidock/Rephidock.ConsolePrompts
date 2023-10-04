6ad0cdf9382d0fb1315f35daa1d9d28d9f248176
Rephi <83082012+Rephidock@users.noreply.github.com>
2023-10-04 22:00:52 +0300

    Created solution and projects

---
+++ src/Rephidock.AdeptConsole.Demo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Rephidock.AdeptConsole;
+
+
+namespace Rephidock.AdeptConsole.Demo;
+
+
+internal class Program {
+
+	static void Main(string[] args) {
+		Console.WriteLine("Hello, World!");
+	}
+
+}
