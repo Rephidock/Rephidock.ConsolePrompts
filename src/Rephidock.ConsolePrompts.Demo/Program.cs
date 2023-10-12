0424653eb093ea237b5d0c522ce7898476c8e6b5
Rephi <83082012+Rephidock@users.noreply.github.com>
2023-10-12 18:42:05 +0300

    Made a small demo

--- src/Rephidock.ConsolePrompts.Demo/Program.cs
+++ src/Rephidock.ConsolePrompts.Demo/Program.cs
@@ -8,7 +8,47 @@ namespace Rephidock.ConsolePrompts.Demo;
 internal class Program {
 
 	static void Main(string[] args) {
-		Console.WriteLine("Hello, World!");
+
+		Console.WriteLine("Welcome to ConsolePrompts demo!");
+
+		//  -======- Strings -======-
+		string name = Prompt.ForString("What is your name", trim: true).DisallowEmpty().Display();
+		Console.WriteLine($"Hello, {name}");
+
+
+		//  -======- Numbers -======-
+		int userAge = Prompt.For<int>("Your age").NoLessThan(1).Display();
+		const int drinkingAge = 21;
+
+		if (userAge >= drinkingAge) {
+			Console.WriteLine("You are of drinking age!");
+		} else {
+			Console.WriteLine("Sorry, you can't have a drink.");
+		}
+
+
+		//  -======- IParsable -======-
+		DateOnly userBirthday = Prompt.For<DateOnly>("When is your birthday").Display();
+		DateOnly today = DateOnly.FromDateTime(DateTime.Now);
+
+		if (userBirthday == today) {
+			Console.WriteLine("Happy birthday!");
+		} else {
+			Console.WriteLine($"Your birthday is on {userBirthday}");
+		}
+
+
+		//  -======- Boolean -======-
+		bool isHexagonEnjoyer = Prompt.ForBool("Do you like hexagons?", defaultValue: true).Display();
+
+		if (isHexagonEnjoyer) {
+			Console.WriteLine("Hexagons are the bestagons!");
+		} else {
+			Console.WriteLine("They are alright I guess...");
+		}
+
+		Console.WriteLine("This concludes the ConsolePrompts demo!");
+
 	}
 
 }
