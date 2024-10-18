de5c18e3fffc275582220cb34a38ab3cff05d38c
Rephi <83082012+Rephidock@users.noreply.github.com>
2024-10-18 18:59:56 +0200

    Finished the demo

--- src/Rephidock.ConsolePrompts.Demo/Program.cs
+++ src/Rephidock.ConsolePrompts.Demo/Program.cs
@@ -18,8 +18,9 @@ internal class Program
 	{
 
 		// - Intro -
-		Console.WriteLine("Welcome to ConsolePrompts demo!");
+		Console.WriteLine("Welcome to ConsolePrompts demo / tutorial!");
 
+		
 		
 
 		// ---------------------------------
@@ -28,13 +29,14 @@ internal class Program
 		StartNextSection("Quick/Basic Use");
 
 
-		// Use Prompt static class for quick prompt creation
+		// Use Prompt static class for quick prompt creation.
 		string name = Prompt.ForString("What is your name", trim: true).DisallowEmpty().Display();
 		Console.WriteLine($"Hello, {name}");
 
 
-		// Numbers supported.
-		// Be careful with floats: "Infinity" and "NaN" are valid inputs unless disabled.
+		// Supports IParsable, incluiding numbers.
+		// Be careful with floats: "Infinity" and "NaN" are valid inputs
+		// (unless disabled with forceFinite parameter).
 		int userAge = Prompt.For<int>("Your age").NoLessThan(1).Display();
 		const int drinkingAge = 21;
 
@@ -48,19 +50,7 @@ internal class Program
 		}
 
 
-		// Supports IParsable
-		DateOnly userBirthday = Prompt.For<DateOnly>("When is your birthday").Display();
-		DateOnly today = DateOnly.FromDateTime(DateTime.Now);
-
-		if (userBirthday == today)
-		{
-			Console.WriteLine("Happy birthday!");
-		}
-		else
-		{
-			Console.WriteLine($"Your birthday is on {userBirthday}");
-		}
-
+		// Futher section has an example with a DateOnly.
 
 		// Booleans are supported and allow single character answers.
 		bool isHexagonEnjoyer = Prompt.ForBool("Do you like hexagons?", defaultValue: true).Display();
@@ -74,7 +64,29 @@ internal class Program
 			Console.WriteLine("They are alright I guess...");
 		}
 
-		
+
+
+
+		// ---------------------------
+		// -======- Null Text -======-
+		// ---------------------------
+		StartNextSection("Null Text");
+
+
+		// Empty or null text displays are supported and show up differently.
+		string mysteryInput = Prompt.ForString().Display();
+		Console.WriteLine($"Mystery Length: {mysteryInput.Length}");
+
+
+		// Common hints are still shown (unless disabled, see in a further section)
+		double mysteryNumericInput = Prompt
+			.For<double>(null, forceFinite: true)
+			.NoLessThan(0)
+			.Display();
+
+		Console.WriteLine($"Mystery Sqaure Root: {Math.Sqrt(mysteryNumericInput)}");
+
+
 
 
 		// --------------------------
@@ -83,7 +95,7 @@ internal class Program
 		StartNextSection("Prompter");
 
 
-		// For multiple prompts or advanced features use Prompter.
+		// For advanced features (explored further) use a Prompter.
 		Prompter prompter = new();
 
 		float favouriteFloat = prompter
@@ -111,88 +123,150 @@ internal class Program
 
 
 
-		// ---------------------------
-		// -======- Null Text -======-
-		// ---------------------------
-		StartNextSection("Null Text");
+		// ---------------------------------------
+		// -======- Further Customisation -======-
+		// ---------------------------------------
+		StartNextSection("Further Customisation");
 
 
-		// Empty or null text displays are supported
-		string mysteryInput = prompter.PromptForString().Display(); 
-		Console.WriteLine($"Mystery Length: {mysteryInput.Length}");
+		// Hints and format prompts is customizable.
+		// Requires a prompter.
+		prompter = new Prompter(autoSetupHints: false);
 
+		// Set up all hints to be displayed
+		// (includes uncommon hints, like "not empty")
+		prompter.SetHintHandlers(PromptHintHandlers.GetAllHandlers());
+		prompter.UnknownHintHandler = PromptHintHandlers.DebugHintHandler;
 
-		// Common hints are still shown (unless disabled)
-		double mysteryNumericInput = prompter
-			.PromptFor<double>(null, forceFinite: true)
+		// Change format of prompts
+		prompter.PromptFormat = "[{1}] {0} := ";
+		prompter.HintSeparator = " & ";
+		prompter.InvalidInputFormat = "Not accepted: {0}";
+
+		// Example prompt
+		float x = prompter
+			.PromptFor<float>("x")
+			.AddTypeHint()
+			.ForceFinite()
+			.OfRange(-1, 1)
 			.NotEqualTo(0)
 			.Display();
 
-		Console.WriteLine($"Mystery Sqaure: {mysteryNumericInput * mysteryNumericInput}");
+		Console.WriteLine($"f(x) = 60 + 10 * {x} = {60 + 10 * x}");
 
 
-		/*
-		//  -======- Custom Validators -======-
+		// Alternatively all hints can be disabled like so
+		prompter = new Prompter(autoSetupHints: false) {
+			UnknownHintHandler = PromptHintHandlers.SkipHintHandler
+		};
+
+
+
+
+		// ----------------------------------
+		// -======- More About Hints -======-
+		// ----------------------------------
+		StartNextSection("More About Hints");
+
+
+		// When a parser a created without hint setup
+		// it has no hint handlers and a debug unknown hint handler.
+		prompter = new Prompter(autoSetupHints: false);
+
+		// Lets add some handlers!
+		prompter.SetHintHandler(PromptHintKeys.BasicText, hint => (hint as PromptHint<string>)?.Payload);
+		prompter.SetHintHandler(PromptHintKeys.StringNotEmptyOrWhitespace, _ => "not empty");
+		prompter.UnknownHintHandler = hint => $"<unknown hint key {hint.Key}>";
+
+		// Lets create a prompt with some hints and see what is displayed
+		_ = prompter
+			.PromptForString("Give me 3 characters")
+			.AddTextHint("3 chrs.")     // This adds a BasicText hint. The given text becomes Payload.
+			.DisallowOnlyWhiteSpace()   // Also adds a StringNotEmptyOrWhitespace hint
+			.OfLength(3)                // Also adds a StringLength hint; Handler not set, unknown will be used.
+
+			// When a prompt is dispalyed all hints are passed through
+			// the prompter's hint handlers based on the hints' keys.
+			.Display();
+
+		// autoSetupHints is true by default
+		// It adds CommonHintHandlers from PromptHintHandlers
+		// and sets a SkipHintHandler as an UnknownHintHandler
+
+		// The Prompt static class uses a prompter with automatically set up hints.
+		
+
+		// Additionally type hints exist.
+		// Prompter can add them automatically or they can be added manually.
+		DateOnly userBirthday = Prompt
+			.For<DateOnly>("When is your birthday")
+			.AddTypeHint()
+			.Display();
+
+		DateOnly today = DateOnly.FromDateTime(DateTime.Now);
+
+		if (userBirthday == today) {
+			Console.WriteLine("Happy birthday!");
+		} else {
+			Console.WriteLine($"Ah, on {userBirthday}. Got it.");
+		}
+
+
+		// You can define your own hints and handlers for them from the ground up
+		const string exampleHintKey = "userExample";
+
+		static string? ExampleHintHandler(PromptHint hint) {
+
+			// Skip unrelated hints if assigned
+			if (hint.Key != exampleHintKey) return null;
+
+			// Handle hints with integer payload
+			if (hint is PromptHint<int> intHint) return $"example.{intHint.Payload}";
+
+			// Handle other hints, incluidng with no payload
+			return "example";
+		}
+
+		prompter.SetHintHandler(exampleHintKey, ExampleHintHandler);
+
+		_ = prompter
+			.PromptForString()
+			.AddHint(new PromptHint(exampleHintKey))
+			.AddHint(new PromptHint<int>(exampleHintKey, 331))
+			.Display();
+
+
+
+
+		// ---------------------------------------
+		// -======- More About Validators -======-
+		// ---------------------------------------
+		StartNextSection("More About Validotrs");
+
+
+		// All restrictions already seen just their internal validator and restrictions.
+
+		// Let's define own validator. Must throw on invalid input.
 		static void MustNotContainLetterAValidator(string input) {
 			if (input.Contains('A', StringComparison.InvariantCultureIgnoreCase)) {
 				throw new PromptInputException("Input must not contain letter 'A'");
 			}
 		}
 
+		// and add it with an appropriate hint
 		string stringWithoutLetterA = Prompt
-			.ForString("I need a sentence without using letter A")
+			.ForString("Give me a sentance", trim: true)
 			.AddValidator(MustNotContainLetterAValidator)
+			.AddTextHint("without letter A")
 			.Display();
 
-		Console.WriteLine($"Given sentence: \"{stringWithoutLetterA}\"");
+		Console.WriteLine($"Given sentence (trimmed) has \"{stringWithoutLetterA.Length}\" characters");
 
-		StartNextSection();
-
-
-		//  -======- Styling -======-
-		TO_BE_REMOVED_PromptStyler.PromptFormat = "[{1}] {0} = ";
-		TO_BE_REMOVED_PromptStyler.InvalidInputFormat = "I can't accept that: {0}";
-		TO_BE_REMOVED_PromptStyler.HintLevel = PromptHintLevel.Verbose;
-
-		Console.WriteLine("f(x) = 60 + 10x");
-
-		float x = Prompt
-			.For<float>("x")
-			.ForceFinite()
-			.OfRange(0, 1)
-			.AddHint("real", PromptHintLevel.Verbose)
-			.Display();
-
-		Console.WriteLine($"f(x) = 60 + 10 * {x} = {60 + 10 * x}");
-
-		StartNextSection();
-
-
-		//  -======- Styling: Type hints -======-
-		TO_BE_REMOVED_PromptStyler.PromptFormat = "{0} ({1}): ";
-		TO_BE_REMOVED_PromptStyler.HintLevel = PromptHintLevel.Standard;
-		TO_BE_REMOVED_PromptStyler.TypeHintsEnabled = true;
-
-		Console.WriteLine("Type hints can also be enabled.");
-
-		float rating = Prompt
-			.For<float>("Give us a rating")
-			.ForceFinite()
-			.OfRange(0, 5)
-			.Display();
-
-		Console.WriteLine($"Given rating: {rating}");
-
-		StartNextSection();
-
-		*/
-		
 
 
 
 		// - Outro -
-		Console.WriteLine();
-		Console.WriteLine();
+		StartNextSection("The End");
 		Console.WriteLine("This concludes the ConsolePrompts demo!");
 		Console.Write("Press any key to exit...");
 		Console.ReadKey(intercept: true);
